5eb8cc2b5f1791f5eda4d2426da38de5ed922b35
pbiecek <przemyslaw.biecek@gmail.com>
2013-06-18 08:56:31 +0200

    fixed UTF8 handling

--- R/loadData.R
+++ R/loadData.R
@@ -7,8 +7,8 @@ extract.data <- function(txt.file, sas.file){
 extract.vardict <- function(sas.file){
   lines <- readLines(sas.file)
   lines <- paste(lines, collapse="\n")
-  chunks <- strsplit(lines, split=";[ \n]*[Ll][Aa][Bb][Ee][Ll][ \n]*")[[1]]
-  chunks <- strsplit(chunks[2], split=";[ \n]*[Ff][Oo][Rr][Mm][Aa][Tt][ \n]*")[[1]][1]
+  chunks <- strsplit(lines, split=";[ \n]*[Ll][Aa][Bb][Ee][Ll][ \n]*", useBytes=TRUE)[[1]]
+  chunks <- strsplit(chunks[2], split=";[ \n]*[Ff][Oo][Rr][Mm][Aa][Tt][ \n]*", useBytes=TRUE)[[1]][1]
   tab <- read.table(textConnection(chunks), sep="=", stringsAsFactors=FALSE)
   tab[,1] <- gsub(gsub(tab[,1], pattern="^ +", replacement=""), pattern=" +$", replacement="")
   tab[,2] <- gsub(gsub(tab[,2], pattern="^ +", replacement=""), pattern=" +$", replacement="")
@@ -19,11 +19,11 @@ extract.vardict <- function(sas.file){
 extract.dict <- function(sas.file){
   lines <- readLines(sas.file)
   lines <- paste(lines, collapse="\n")
-  chunks <- strsplit(lines, split=";[ \n]*[Vv][Aa][Ll][Uu][Ee][ \n]*")[[1]]
+  chunks <- strsplit(lines, split=";[ \n]*[Vv][Aa][Ll][Uu][Ee][ \n]*", useBytes=TRUE)[[1]]
   chunks <- chunks[2:(length(chunks)-1)]
   res <- list()
   for (i in seq_along(chunks)) {
-    vn <- gsub(strsplit(chunks[i], split="[ \n]")[[1]][1], pattern="\\$", replacement="")
+    vn <- gsub(strsplit(chunks[i], split="[ \n]", useBytes=TRUE)[[1]][1], pattern="\\$", replacement="")
     tmp <- gsub(chunks[i], pattern="^[^\n ]*[\n ]*", replacement="")
     tab <- read.table(textConnection(tmp), sep="=", stringsAsFactors=FALSE)
     tab2 <- tab[,2]
