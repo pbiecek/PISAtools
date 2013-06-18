71cbcda85e196484dfb9c503014abb3463d9c4a2
pbiecek <przemyslaw.biecek@gmail.com>
2013-06-18 11:11:05 +0200

    remove empty spaces around variable names

--- R/loadData.R
+++ R/loadData.R
@@ -5,9 +5,9 @@ extract.data <- function(txt.file, sas.file){
   dat
 }
 extract.vardict <- function(sas.file){
-  lines <- readLines(sas.file)
-  lines <- paste(lines, collapse="\n")
-  chunks <- strsplit(lines, split=";[ \n]*[Ll][Aa][Bb][Ee][Ll][ \n]*", useBytes=TRUE)[[1]]
+  alines <- readLines(sas.file, encoding="UTF-8")
+  alines <- paste(alines, collapse="\n")
+  chunks <- strsplit(alines, split=";[ \n]*[Ll][Aa][Bb][Ee][Ll][ \n]*", useBytes=TRUE)[[1]]
   chunks <- strsplit(chunks[2], split=";[ \n]*[Ff][Oo][Rr][Mm][Aa][Tt][ \n]*", useBytes=TRUE)[[1]][1]
   tab <- read.table(textConnection(chunks), sep="=", stringsAsFactors=FALSE)
   tab[,1] <- gsub(gsub(tab[,1], pattern="^ +", replacement=""), pattern=" +$", replacement="")
@@ -17,7 +17,7 @@ extract.vardict <- function(sas.file){
   tab2
 }
 extract.dict <- function(sas.file){
-  lines <- readLines(sas.file)
+  lines <- readLines(sas.file, encoding="UTF-8")
   lines <- paste(lines, collapse="\n")
   chunks <- strsplit(lines, split=";[ \n]*[Vv][Aa][Ll][Uu][Ee][ \n]*", useBytes=TRUE)[[1]]
   chunks <- chunks[2:(length(chunks)-1)]
@@ -26,6 +26,8 @@ extract.dict <- function(sas.file){
     vn <- gsub(strsplit(chunks[i], split="[ \n]", useBytes=TRUE)[[1]][1], pattern="\\$", replacement="")
     tmp <- gsub(chunks[i], pattern="^[^\n ]*[\n ]*", replacement="")
     tab <- read.table(textConnection(tmp), sep="=", stringsAsFactors=FALSE)
+    tab[,1] <- gsub(gsub(tab[,1], pattern="^ +", replacement=""), pattern=" +$", replacement="")
+    tab[,2] <- gsub(gsub(tab[,2], pattern="^ +", replacement=""), pattern=" +$", replacement="")
     tab2 <- tab[,2]
     names(tab2) <- tab[,1]
     res[[vn]] <- tab2
