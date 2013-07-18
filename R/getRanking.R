4c5aba6b4de9f57bf01534a8e69b53589e97a293
pbiecek <przemyslaw.biecek@gmail.com>
2013-07-18 13:02:13 +0200

    now also weighted averages may be sorted

--- R/getRanking.R
+++ R/getRanking.R
@@ -1,11 +1,13 @@
 # weights are single col
-getWeightedAverages <- function(values, groups, weights = 1) {
+getWeightedAverages <- function(values, groups, weights = 1, sort = NA) {
   require(plyr)
   require(reshape2)
   df <- data.frame(values, weights, groups)
   mdf <- melt(df, id=c("groups","weights"), measure=colnames(df)[1:(ncol(df)-2)])
   rres <- ddply(mdf, .(groups, variable), summarize, avg = weighted.mean(value, weights, na.rm=T)) 
-  acast(rres, groups~variable, value.var="avg")
+  tdf <- acast(rres, groups~variable, value.var="avg")
+  if (!is.na(sort)) tdf <- tdf[order(tdf[,sort], decreasing=TRUE),,drop=FALSE]
+  tdf
 }
 
 getRanking <- function(values, groups, weights = 1, sort = NA) {
