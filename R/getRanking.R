7b9e1624b00e14b0c59ec2128e3087faad006cec
pbiecek <przemyslaw.biecek@gmail.com>
2013-07-18 12:08:01 +0200

    fix for missing data

--- R/getRanking.R
+++ R/getRanking.R
@@ -2,7 +2,7 @@
 getWeightedAverages <- function(values, groups, weights = 1) {
   unclass(by(cbind(values, weights), groups, 
              function(x) apply(as.matrix(x[,-ncol(x)]), 2, 
-                               function(y) weighted.mean(y, x[,ncol(x)]))))
+                               function(y) weighted.mean(y, x[,ncol(x)], na.rm=TRUE))))
 }
 
 getRanking <- function(values, groups, weights = 1) {
