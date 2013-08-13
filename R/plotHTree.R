8c87453a25de8d4d5e3c6f3c01b4d4e1c667a107
pbiecek <przemyslaw.biecek@gmail.com>
2013-08-13 18:49:14 +0200

    man page for plotHTree function

--- R/plotHTree.R
+++ R/plotHTree.R
@@ -1,11 +1,11 @@
-plotHTree <- function(level1, level2, sizes=rep(1, length(level2)) labels, mar=c(2,5,20,2), plotit=rep(TRUE, length(level2))) {
+plotHTree <- function(level1, level2,  labels, sizes=rep(1, length(level2)), mar=c(2,5,20,2), plotit=rep(TRUE, length(level2))) {
     par(mar=mar)
     plot(seq_along(level2), level2, pch=19, cex=sizes, las=1, xaxt="n", xlab="", ylab="", main="", type="n", yaxt="n")
-    axis(2, las=1, seq(round(min(level2), -1), max(level2), 10))
-    axis(3, seq_along(level2), labels, las=2)
+    axis(2, las=1, seq(round(min(level2, na.rm=TRUE), -1), max(level2, na.rm=TRUE), 10))
+    axis(3, seq_along(labels), labels, las=2)
 
     for (i in seq_along(level1)) {
-      tx <- grep(names(level2), pattern=paste("^", names(level1)[1], sep=""))
+      tx <- grep(names(level2), pattern=paste("^", names(level1)[i], sep=""))
       lines(range(tx), level1[i]*c(1,1))
       for (txx in tx) {
         if (plotit[txx]) {
