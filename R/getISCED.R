9fed1483c40d4e080d2875b9b88667cc0cfeb11c
pbiecek <przemyslaw.biecek@gmail.com>
2013-08-14 15:13:17 +0200

    man page for getISCED and fix for tolower in the function

--- R/getISCED.R
+++ R/getISCED.R
@@ -1,7 +1,7 @@
 getISCED <- function(..., base) {
   base <- sapply(strsplit(base, split="[<>]"), function(x) {
     if (is.na(x[1])) return(NA)
-    if (substr(x[1], 1, 7) == "Did Not") return ("ISCED level 0")
+    if (tolower(substr(x[1], 1, 7)) == "did not") return ("ISCED level 0")
     x[length(x)]
   })
   addList <- rev(list(...))  
