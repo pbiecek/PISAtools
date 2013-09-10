0e1a95cad84b3abe1ddda31aa634a07fd0cc8c27
pbiecek <przemyslaw.biecek@gmail.com>
2013-09-10 13:08:17 +0200

    fixed classes

--- R/df2json.R
+++ R/df2json.R
@@ -1,5 +1,5 @@
 escapeIfNotNumber <- function(x) {
-  if (is.null(dim(x))) {
+  if (is.null(dim(x)) || min(dim(x))==1) {
     sapply(x, function(v) ifelse (class(v) == "numeric", v, paste0('"', v, '"')))
   } else {
     apply(x, 1:2, function(v) ifelse (class(v) == "numeric", v, paste0('"', v, '"')))
@@ -8,7 +8,7 @@ escapeIfNotNumber <- function(x) {
 
 df2json <- function(df) {
   vnam <- paste0('"', colnames(df), '":')
-  rows <- apply(df, 1, function(x) paste0(vnam, escapeIfNotNumber(x), collapse= ", "))
+  rows <- sapply(1:nrow(df), function(i) paste0(vnam, escapeIfNotNumber(df[i,]), collapse= ", "))
   rows <- paste0("{", rows, "}")
   paste0("[", paste(rows, collapse=",\n"), "]")
 }
