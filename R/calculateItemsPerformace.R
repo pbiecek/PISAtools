c219bcc6822ff5e21d9e8776a93bedc98aa27fc7
pbiecek <przemyslaw.biecek@gmail.com>
2013-09-18 18:21:43 +0200

    item performance for countries

---
+++ R/calculateItemsPerformace.R
@@ -0,0 +1,19 @@
+calculateItemsPerformace <- function(itemNames, dataset, naLevels = c("7", "8"), cntVal = "CNT", weightVal = "W_FSTUWT") {
+    res <- matrix(NA, length(itemNames), nlevels(dataset[,cntVal]))
+    rownames(res) <- itemNames
+    colnames(res) <- levels(dataset[,cntVal])
+    for (i in seq_along(itemNames)) {
+        item <- dataset[,grep(colnames(dataset), pattern=substr(itemNames[i],1,7))]
+        
+        for (j in seq_along(naLevels)) {
+            item[which(item == naLevels[j])] <- NA
+        }
+        
+        maxAns  <- item == as.character(max(as.numeric(as.character(item)), na.rm=TRUE))
+        perfromance <- prop.table(unclass(by(dataset[,weightVal], list(dataset[,cntVal],  maxAns), base::sum, na.rm=TRUE)), 1)
+        
+        res[i,] <-  round(perfromance[,2] * 100,1)
+    }
+    res
+}
+
