f83beef7565d568a3778ea81339fcfc520c75b35
pbiecek <przemyslaw.biecek@gmail.com>
2013-09-19 10:39:39 +0200

    item performance in groups

--- R/calculateItemsPerformace.R
+++ R/calculateItemsPerformace.R
@@ -3,16 +3,16 @@ calculateItemsPerformace <- function(itemNames, dataset, naLevels = c("7", "8"),
     rownames(res) <- itemNames
     colnames(res) <- levels(dataset[,cntVal])
     for (i in seq_along(itemNames)) {
-        item <- dataset[,grep(colnames(dataset), pattern=substr(itemNames[i],1,7))]
+        item <- dataset[,grep(colnames(dataset), pattern=substr(itemNames[i],1,7))[1]]
         
         for (j in seq_along(naLevels)) {
             item[which(item == naLevels[j])] <- NA
         }
         
         maxAns  <- item == as.character(max(as.numeric(as.character(item)), na.rm=TRUE))
-        perfromance <- prop.table(unclass(by(dataset[,weightVal], list(dataset[,cntVal],  maxAns), base::sum, na.rm=TRUE)), 1)
+        performance <- prop.table(unclass(by(dataset[,weightVal], list(dataset[,cntVal],  maxAns), base::sum, na.rm=TRUE)), 1)
         
-        res[i,] <-  round(perfromance[,2] * 100,1)
+        res[i,] <-  round(performance[,2] * 100,1)
     }
     res
 }
---
+++ R/calculateItemsPerformaceInGroups.R
@@ -0,0 +1,18 @@
+calculateItemsPerformaceInGroups <- function(itemNames, dataset, grVal, cntVal = "CNT", weightVal = "W_FSTUWT", naLevels = c("7", "8")) {
+  res <- array(NA, c(length(itemNames), nlevels(dataset[,cntVal]), nlevels(dataset[,grVal])))
+  dimnames(res) <- list(itemNames, levels(dataset[,cntVal]), levels(dataset[,grVal]))
+  
+  for (i in seq_along(itemNames)) {
+    item <- dataset[,grep(colnames(dataset), pattern=substr(itemNames[i],1,7))[1]]
+    
+    for (j in seq_along(naLevels)) {
+      item[which(item == naLevels[j])] <- NA
+    }
+    
+    maxAns  <- item == as.character(max(as.numeric(as.character(item)), na.rm=TRUE))
+    performance <- prop.table(unclass(by(dataset[,weightVal], list(dataset[,cntVal], dataset[,grVal], maxAns), base::sum, na.rm=TRUE)), 1:2)
+    
+    res[i,,] <-  round(performance[,,2] * 100,1)
+  }
+  res
+}
