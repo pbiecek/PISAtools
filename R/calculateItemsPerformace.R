dfaa596033e5adc101adc3ebf30ac9d357528397
pbiecek <przemyslaw.biecek@gmail.com>
2013-11-25 18:10:30 +0100

    give strict item names if needed

--- R/calculateItemsPerformace.R
+++ R/calculateItemsPerformace.R
@@ -1,13 +1,20 @@
-calculateItemsPerformace <- function(itemNames, dataset, naLevels = c("7", "8"), cntVal = "CNT", weightVal = "W_FSTUWT") {
+calculateItemsPerformace <- function(itemNames, dataset, naLevels = c("7", "8"), cntVal = "CNT", weightVal = "W_FSTUWT", exactNameMatch = FALSE) {
     res <- matrix(NA, length(itemNames), nlevels(dataset[,cntVal]))
     rownames(res) <- itemNames
     colnames(res) <- levels(dataset[,cntVal])
     for (i in seq_along(itemNames)) {
+      if (exactNameMatch) {
+        item <- dataset[,itemNames[i]]
+      } else {
         item <- dataset[,grep(colnames(dataset), pattern=substr(itemNames[i],1,7))[1]]
-        
-        for (j in seq_along(naLevels)) {
-            item[which(item == naLevels[j])] <- NA
-        }
+      }
+      
+      if (length(naLevels) > 0)       
+          item[which(item %in% naLevels[j])] <- NA
+      
+#      for (j in seq_along(naLevels)) {
+#            item[which(item == naLevels[j])] <- NA
+#        }
         
         maxAns  <- item == as.character(max(as.numeric(as.character(item)), na.rm=TRUE))
         performance <- prop.table(unclass(by(dataset[,weightVal], list(dataset[,cntVal],  maxAns), base::sum, na.rm=TRUE)), 1)
