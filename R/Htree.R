a5def506746ea80c8ad88c990b5152561c46fa9f
Przemyslaw Biecek <przemyslaw.biecek@gmail.com>
2013-12-20 02:08:36 +0100

    slope graph

--- R/Htree.R
+++ R/Htree.R
@@ -19,6 +19,27 @@ plotFlatHtree <- function(flatHtree, x, y, size, label, color, range) {
     )
 }
 
+plotSlopeHtree <- function(val1, val2, lab1, lab2, col1, col2, rang=range(c(val1, val2), na.rm=TRUE)) {
+  flatHtree <- rbind(
+              data.frame(cnt = 0, avg = val1, lab= lab1, color=col1),
+              data.frame(cnt = 1, avg = val2, lab= lab2, color=col2))
+  
+  ggplot(data = flatHtree, aes(x = cnt, y = avg, group=lab, color=color)) + 
+    geom_line() + 
+    geom_text(aes(label = lab, x=cnt*1.4 - 0.2 , hjust = 1-cnt, size=0.5)) +
+    theme_bw()+
+    scale_color_brewer(palette = "RdYlBu") + 
+    scale_x_continuous("", limits = c(-3,4)) + 
+    scale_y_continuous("", limits = rang) + 
+    theme(plot.title = element_text(face = "bold", size = 14), 
+          axis.title.x = element_blank(), axis.ticks.x = element_blank(), 
+          axis.text.x = element_blank(), axis.title.y = element_text(face = "bold", size = 12, angle = 90), panel.grid.major.x = element_blank(), 
+          panel.grid.minor.x = element_blank(), legend.position = "none", 
+          legend.title = element_blank(), legend.text = element_text(size = 12), 
+          panel.border = element_rect(linetype = "dotted", colour = "white"), legend.key = element_blank())
+  
+}
+
 getHFlatAverages <- function(Htree, vname, cname, level=0) {
   inds <- which(sapply(Htree, class) == "Hgroup")
   pre <- NULL
