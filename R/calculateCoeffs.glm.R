29940c3ba8edb6aefbc6391e1af2905db1eb33ee
Przemyslaw Biecek <przemyslaw.biecek@gmail.com>
2014-03-03 21:15:03 +0100

    fix, when weights are too large

--- R/calculateCoeffs.glm.R
+++ R/calculateCoeffs.glm.R
@@ -1,5 +1,5 @@
 calculateCoeffs.glm <- function(model, datas, weigths = NULL, fweigth) {
-  mres <- glm(model, datas, weights=datas[,fweigth], family="binomial")
+  mres <- glm(model, datas, weights=datas[,fweigth]*2/max(datas[,fweigth]), family="binomial")
   starters <- mres$coefficients
   
   if (is.null(weigths)) {
@@ -7,9 +7,9 @@ calculateCoeffs.glm <- function(model, datas, weigths = NULL, fweigth) {
     se <- NULL
   } else {
     mresAll <- sapply(seq_along(weigths), function (i) {
-      datas$wei <- datas[,weigths[i]]
+      datas$wei <- datas[,weigths[i]]*2/max(datas[,weigths[i]])
       mresW <- glm(model, datas, weights=wei, family="binomial")
-      smresW$coefficients
+      mresW$coefficients
     })
     se <- sqrt(rowMeans((mresAll - starters)^2))/sqrt(length(weigths))
   }
